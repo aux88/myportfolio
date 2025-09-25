5ff752811fffcd0b3a992282498e10a406b83ea2
yoshimi.satou <youhes@gmail.com>
2025-09-25 19:35:46 +0900

    テキストを写真と同様に変えられるように修正

--- src/main.js
+++ src/main.js
@@ -15,30 +15,32 @@ const nextBtn = document.getElementById('next');
 const thumbnails = document.querySelectorAll('.thumbnail');
 const modalImg = document.getElementById('modal-image');
 
+const captionText = document.getElementById('image-caption');
+
 // 各ギャラリーの画像を定義（ここは動的でもOK）
 const galleries = {
    gallery1: [
-      '/src/images/curry/IMG_1366.jpg',
-      '/src/images/curry/IMG_2290.jpg',
-      '/src/images/curry/IMG_2922.jpg',
-      '/src/images/curry/IMG_3127.jpg',
-      '/src/images/curry/IMG_5213.jpg',
-      '/src/images/curry/IMG_6600.jpg',
-      '/src/images/curry/IMG_9861.jpg',
+      { src: '/src/images/curry/IMG_1366.jpg', caption: '月島 年輪のスープカレー' },
+      { src: '/src/images/curry/IMG_2290.jpg', caption: '仙川 KICK BACK CARE 中居屋十兵衛カレー' },
+      { src: '/src/images/curry/IMG_2922.jpg', caption: '下北沢 ポニピリカ スープカレー' },
+      { src: '/src/images/curry/IMG_3127.jpg', caption: '下北沢 カルパシのカレー' },
+      { src: '/src/images/curry/IMG_5213.jpg', caption: '上北沢 ハチドリのスープカレー' },
+      { src: '/src/images/curry/IMG_6600.jpg', caption: '下北沢 カレーの惑星' },
+      { src: '/src/images/curry/IMG_9861.jpg', caption: '上北沢 ハチドリのスープカレー' },
    ],
    gallery2: [
-      '/src/images/coffee/IMG_2680.jpg',
-      '/src/images/coffee/IMG_3161.jpg',
-      '/src/images/coffee/IMG_3297.jpg',
-      '/src/images/coffee/IMG_4658.jpg',
-      '/src/images/coffee/rowbeans.jpg',
+      { src: '/src/images/coffee/IMG_2680.jpg', caption: 'ラテアートのお魚がかわいい@伊勢' },
+      { src: '/src/images/coffee/IMG_3161.jpg', caption: 'エチオピア イルガチェフェ@ラテグラフィック' },
+      { src: '/src/images/coffee/IMG_3297.jpg', caption: 'ランチデザートアップルパイにコーヒー＠二子玉川' },
+      { src: '/src/images/coffee/IMG_4658.jpg', caption: '数量限定のアイスコーヒー@ラテグラフィック' },
+      { src: '/src/images/coffee/rowbeans.jpg', caption: 'エリトリアのお土産の生豆を自分で焙煎しました' },
    ],
    gallery3: [
-      '/src/images/homemade/IMG_3765.jpg',
-      '/src/images/homemade/IMG_3759.jpg',
-      '/src/images/homemade/IMG_3760.jpg',
-      '/src/images/homemade/IMG_3761.jpg',
-      '/src/images/homemade/IMG_3762.jpg',
+      { src: '/src/images/homemade/IMG_3765.jpg', caption: '手作りのチキンカレー' },
+      { src: '/src/images/homemade/IMG_3759.jpg', caption: '近くのカフェの自社農園で育てたイタリアントマトを買ってきました' },
+      { src: '/src/images/homemade/IMG_3760.jpg', caption: '生のイタリアントマトでカレーを作るのは初めてです' },
+      { src: '/src/images/homemade/IMG_3761.jpg', caption: 'カレールーは使わずに、スパイスで作ります' },
+      { src: '/src/images/homemade/IMG_3762.jpg', caption: '一食分だけチキンを入れて少し煮込みます' },
    ]
 };
 
@@ -75,7 +77,9 @@ closeBtn.addEventListener('click', () => {
 
 // 画像表示
 function showImage(index) {
-   modalImg.src = currentGallery[index];
+   const item = currentGallery[index];
+   modalImg.src = item.src;
+   captionText.textContent = item.caption;  // ← 説明文をセット
    current.textContent = index + 1;
 
    // ボタン活性制御
